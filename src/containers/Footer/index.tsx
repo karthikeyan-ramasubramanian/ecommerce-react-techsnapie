ba8557db03b27a4185ad5295f1ae7073dd23b386
anto-mervin <antomervin2003.edu@gmail.com>
2023-04-03 18:38:27 +0530

    anto

---
+++ src/containers/Footer/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC, ReactElement } from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+export const Footer: FC = (): ReactElement => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "#262626",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h5">Ecommerce App</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`${new Date().getFullYear()} | Techsnapie Solutions `}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,19 +1,20 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import { useIsLoggedIn } from "../config/hooks";
-import Navbar from '../containers/Navbar';
-import { Box } from '@mui/material';
+import Navbar from "../containers/Navbar";
+import { Box } from "@mui/material";
+import { Footer } from "../containers/Footer";
 
 export default function AppLayout() {
+  const isLoggedIn = useIsLoggedIn();
 
-    const isLoggedIn = useIsLoggedIn();
-
-    /*if (isLoggedIn === null) return null;
+  /*if (isLoggedIn === null) return null;
     else if (isLoggedIn === false) return <Navigate replace to="/login"/>;*/
 
-    return (
-        <>
-            <Navbar/>
-            <Outlet/>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
